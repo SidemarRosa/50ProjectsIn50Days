9c5da99c5413bb24aee87c18d505fc5306ee8056
Sidemar Rosa <102702961+SidemarRosa@users.noreply.github.com>
2023-08-23 20:00:56 -0300

    Completo
    Efeito de transição wave feito com Js e detalhes finais.

--- Day 9 - Form Wave/script.js
+++ Day 9 - Form Wave/script.js
@@ -1,8 +1,8 @@
-const label = document.querySelectorAll('.form-control label');
+const labels = document.querySelectorAll('.form-control label')
 
-labels.forEach( label => {
-      label.innerHTML = label.innerText
+labels.forEach(label => {
+    label.innerHTML = label.innerText
         .split('')
-        .map((letter, idx) => `<span>${letter}</span>` )
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
         .join('')
-});
+})
