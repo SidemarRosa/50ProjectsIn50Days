8808704af269236cbc63be6d9f76fd12b8cdf039
Sidemar Rosa <102702961+SidemarRosa@users.noreply.github.com>
2023-08-30 15:55:44 -0300

    Day 11 - completo
    Aplicação de api usando Js

---
+++ Day 11 -  Dad jokes/script.js
@@ -0,0 +1,30 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+generateJoke();
+jokeBtn.addEventListener('click', generateJoke)
+
+
+async function generateJoke(){
+      const config = {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json'
+        }
+};
+
+
+const res = await fetch('https://icanhazdadjoke.com', config);
+
+const data = await res.json()
+
+jokeEl.innerHTML = data.joke
+
+    //usando .then
+    /*fetch('https://icanhazdadjoke.com', config)
+        .then((res) => res.json())
+        .then((data) => {
+        jokeEl.innerHTML = data.joke
+        });    
+    */
+}
