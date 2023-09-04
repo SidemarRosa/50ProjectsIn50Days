4f9891dae8f99e3693bba3b9f32b7a887e14be19
Sidemar Rosa <102702961+SidemarRosa@users.noreply.github.com>
2023-09-04 20:53:13 -0300

    Day 14 - Random pick
    Projetinho bacana de escolha aleatória util para sorteios etc feito com JavaScript com conceitos de Split e arrays

--- Day 14 - Random pick/script.js
+++ Day 14 - Random pick/script.js
@@ -1,10 +1,68 @@
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const container = document.querySelector('.container');
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
 
-left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
-left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
-right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
-right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
+})
+
+function createTags(input){
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add("tag")
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    })
+}
+
+function randomSelect() {
+    const times = 30
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+        if (randomTag !== undefined) {
+            highlightTag(randomTag)
+    
+            setTimeout(() => {
+                unHighlightTag(randomTag)
+            }, 100)
+        }
+    }, 100);
+
+    setTimeout(() =>{
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+            highlightTag(randomTag);
+
+        }, 100);
+    }, times * 100)
+}
+
+function pickRandomTag(){
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
+}
+
 
 
